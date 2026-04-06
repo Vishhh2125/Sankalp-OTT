f8db679f2906bf9a33e6799abf96dfdf81c1b5ca
Vishnu Uplenchwar <167734203+Vishhh2125@users.noreply.github.com>
2026-04-06 16:36:40 +0530

    Revert "frontend_admin_panel"

--- backend/src/queue.js
+++
@@ -1,8 +0,0 @@
-const { Queue } = require('bullmq');
-const redis = require('./config/redis');
-
-const transcodeQueue = new Queue('transcode-queue', {
-  connection: redis,
-});
-
-module.exports = transcodeQueue;
